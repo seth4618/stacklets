be216545833050a384123912dddc850691aef824
Tianyuan Ding <tding@andrew.cmu.edu>
2015-11-06 14:06:45 -0500

    Add #define DUI/EUI/SENDI

--- compiler-example/userint.h
+++ compiler-example/userint.h
@@ -3,18 +3,17 @@
 
 #ifdef SIM
 
-#define DUI(x)	asm volatile("%0x822222 " ## x);
-EUI
-SENDI
+#define DUI(x)	asm volatile("%0x06 %[arg]",  : [arg] "=r" (x))
+#define EUI(x)	asm volatile("%0x07 %[arg]",  : [arg] "=r" (x))
+#define SENDI(x, y)	asm volatile("%0x16 %[arg] %[arg2]",  : [arg] "=r" (x) : [arg2] "=r" (y))
+
 #define POLL()
 
 #endif
 
 #define DUI(x)	dui(x)
-EUI
-SENDI
+#define EUI(x) 	eui(x)
+#define SENDI(x, y) sendi(x, y)
 #define POLL() poll()
-
-#endif
 
 #endif
