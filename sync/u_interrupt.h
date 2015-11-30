151260ece5ec857f4f888485308aa07d2b826022
Saurabh Kadekodi <saukad@h0.smr.smr-filesystem.marmot.pdl.cmu.local>
2015-11-30 09:46:55 -0500

    fixed gem5 hooks in myapp

--- sync/u_interrupt.h
+++ sync/u_interrupt.h
@@ -12,17 +12,33 @@ void init_uint();
 
 #ifdef SIM
 
-#define DUI(x)	asm volatile("%0x6000 %[arg]",  : [arg] "=r" (x))
-#define EUI(x)	asm volatile("%0x6001 %[arg]",  : [arg] "=r" (x))
-#define SENDI(x, y)	asm volatile("%0x6002 %[arg] %[arg2]",  : [arg] "=r" (x) : [arg2] "=r" (y))
-#define POLL() 
+/*
+ * The SIM flag denotes that myapp is being run on simulated hardware (which in
+ * this case is via gem5.
+ */
+//#define DUI(x)	asm volatile("%0x6000 %[arg]",  : [arg] "=r" (x))
+//#define EUI(x)	asm volatile("%0x6001 %[arg]",  : [arg] "=r" (x))
+//#define SENDI(x, y)	asm volatile("%0x6002 %[arg] %[arg2]",  : [arg] "=r" (x) : [arg2] "=r" (y))
+//#define POLL()
+#define DUI(x)  stacklet_dui(x)
+#define EUI(x)  stacklet_eui(x)
+#define SENDI(x, y) stacklet_sendi(x, y)
+//#define SENDI(x, y) sendI(x, y)
+//#define POLL() poll()
+#define POLL()  printf("polling from within gem5...\n");
+#define RETULI()  stacklet_retuli()
+//#define RETULI()  return
 
 #else
 
+/*
+ * The macros below are when running myapp as a regular C program.
+ */
 #define DUI(x)	dui(x)
 #define EUI(x) 	eui(x)
 #define SENDI(x, y) sendI(x, y)
 #define POLL() poll()
+#define RETULI()  return
 
 #endif
 
