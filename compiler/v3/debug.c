b8d547cd5accbb7f4710a43de67f500b312038fd
seth <seth@cmu.edu>
2015-12-01 23:54:37 -0500

    add dprintLine

--- compiler/v3/debug.c
+++ compiler/v3/debug.c
@@ -1,4 +1,7 @@
+#include <stdarg.h>
 #include "debug.h"
+
+extern __thread int threadId;
 
 void bp(void) {}
 
@@ -8,3 +11,21 @@ void die(char* str)
     fprintf(stderr, "Error: %s\n", str);
     exit(-1);
 }
+
+void
+dprintLine(char* fmt, ...)
+{
+  va_list ap;
+  char buffer[64];
+  sprintf(buffer, "%d:%s", threadId, fmt);
+
+  va_start(ap,fmt);
+  vfprintf(stderr, buffer, ap);
+  fflush(stderr);
+  va_end(ap);
+}
+
+// Local Variables:
+// mode: c           
+// c-basic-offset: 4
+// End:
