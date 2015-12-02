437949ea32629ae4a3f230bae11785a2adf4640f
seth <seth@cmu.edu>
2015-12-01 23:57:06 -0500

    remove single seedStackLock, keep track of threadid that owns a seed, hide the lock/unlock, change init to know number of threads we are running on, added emacs local var stuff, added peekSeed to check top of seedstack without exposing implementation

--- compiler/v3/seedStack.h
+++ compiler/v3/seedStack.h
@@ -11,8 +11,31 @@ typedef struct Seed {
     int id;
 } Seed;
 
-Seed* seedDummyHead;
-void seedStackInit();
+// must be called by MAIN thread (AND ONLY MAIN THREAD)
+void seedStackInit(int numThreads);
+
+// allocate and initialize a seed
 Seed* initSeed(void* adr, void* sp);
-void pushSeed(Seed* seed);
-void popSeed(Seed* seed);
+
+// push an initialized seed onto tid's stack.  if lock==1, grab lock first
+void pushSeed(Seed* seed, int tid, int lock);
+
+// pop top seed from tid's stack.  if unlock==1, release lock afterwards
+void popSeed(int tid, int unlock);
+
+// get and return top seed from tid's stack.  DON'T change ptrs if
+// returning a non-null seed, then lock for tid's stack will be
+// grabbed. Otherwise not
+Seed* peekSeed(int tid);
+
+// release lock on tid's seedStack
+void seedStackUnlock(int tid);
+
+// grab lock on tid's seedStack
+void seedStackLock(int tid);
+
+
+// Local Variables:
+// mode: c           
+// c-basic-offset: 4
+// End:
