a48c9e3d91ce302184b8e04c705bbd7f18c60cb5
seth <seth@cmu.edu>
2015-12-03 12:44:50 -0500

    change all refs to spinlock refs.  See if I am calculating stubbase correctly.

--- compiler/v3/myfib.c
+++ compiler/v3/myfib.c
@@ -7,18 +7,34 @@
 #include <assert.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include "myassert.h"
 
 #ifdef DEBUG
 volatile int line;
-pthread_mutex_t lineLock;
+SpinLockType lineLock;
 int lineReturned[300000000];
-pthread_mutex_t lineReturnedLock;
+SpinLockType lineReturnedLock;
 #endif
 
 #ifdef BENCHMARK
 struct timeval startTime;
 struct timeval endTime;
 #endif
+
+void
+checkStubBase(int where)
+{
+    void* mysp;
+    asm("movq %%rsp, %[var]" : [var] "=r" (mysp));
+    void* osb = getOldStubBase(mysp);
+
+    // check that stubBase is correct
+    if (stubBase != osb) {
+	dprintLine("%d: Subbases not equal!  old:%p  new:%p\n", where, stubBase, osb);
+	assert(0);
+    }
+}
+
 
 int result[] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,
                 4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,
@@ -45,18 +61,23 @@ fib(void* F)
     Foo* f = (Foo *)F;
     Registers saveArea; //XXX can this make sure saveArea is on the stack
 
-    DPL(">fib(%d)\n", f->input);
+    void* mysp;
+    asm("movq %%rsp, %[var]" : [var] "=r" (mysp));
+    //dprintLine(">fib(%d)  at %p\n", f->input, mysp);
+
+    checkStubBase(1);
 
     if (f->input <= 2)
     {
 
 #ifdef DEBUG
-        pthread_mutex_lock(&lineLock);
+        mySpinLock(&lineLock);
         DEBUG_PRINT("[threadId = %ld, n = %d, line = %d]\n",
             threadId, f->input, line++);
-        pthread_mutex_unlock(&lineLock);
+        mySpinUnlock(&lineLock);
 #endif
 
+	//dprintLine("<fib(%d) = %d  at %p\n", f->input, 1, mysp);
         f->output = 1;
         return;
     }
@@ -69,11 +90,11 @@ fib(void* F)
 #endif
 
 #ifdef DEBUG
-    pthread_mutex_lock(&lineLock);
+    mySpinLock(&lineLock);
     int volatile localLine = line++;
     DEBUG_PRINT("[threadId = %ld, n = %d, line = %d]\n",
         threadId, f->input, localLine);
-    pthread_mutex_unlock(&lineLock);
+    mySpinUnlock(&lineLock);
 #endif
 
     Foo* a = (Foo *)calloc(1, sizeof(Foo));
@@ -106,7 +127,8 @@ fib(void* F)
     fib(a);
     restoreRegisters(); // may not need to as we already used "volatile" on
                         // "firstChildReturnAdr"
-    seedStackLock(threadId);
+    seedStackLock(ptid);
+    checkStubBase(2);
     goto *firstChildReturnAdr;
 
  FirstChildDoneNormally:
@@ -114,8 +136,11 @@ fib(void* F)
     popSeed(ptid, 1);	       /* also releases lock */
     // ====================================
 
+    checkStubBase(3);
+
     fib(b);
     f->output = a->output + b->output;
+    //dprintLine("<fib(%d) = %d  at %p\n", f->input, f->output, mysp);
     return;
 
 SecondChildSteal: // We cannot make function calls here!
@@ -125,6 +150,7 @@ SecondChildSteal: // We cannot make function calls here!
     firstChildReturnAdr = &&FirstChildDone;
     syncCounter = 2;
     stubBase = localStubBase;	/* SCG:what is local stubbase? */
+    checkStubBase(4);
     saveRegisters();
 #ifdef TRACKER
     __sync_add_and_fetch(&trackingInfo.fork, 1);
@@ -136,7 +162,7 @@ FirstChildDone:
     // stacklet ===========================
     dprintLine("fib(%d) has first child returned\n", f->input);
     restoreRegisters();
-    seedStackUnlock(threadId);
+    seedStackUnlock(ptid);
     {
         int localSyncCounter = __sync_sub_and_fetch(&syncCounter, 1);
         if (localSyncCounter != 0)
@@ -149,15 +175,17 @@ FirstChildDone:
     restoreRegisters();
     // ====================================
 
+    checkStubBase(5);
+
 #ifdef TRACKER
     __sync_add_and_fetch(&trackingInfo.firstReturn, 1);
 #endif
 
 #ifdef DEBUG
-    pthread_mutex_lock(&lineReturnedLock);
+    mySpinLock(&lineReturnedLock);
     assert(lineReturned[localLine] == 0);
     lineReturned[localLine] = 1;
-    pthread_mutex_unlock(&lineReturnedLock);
+    mySpinUnlock(&lineReturnedLock);
 #endif
 
     f->output = a->output + b->output;
@@ -177,15 +205,17 @@ SecondChildDone: // We cannot make function calls before we confirm first child
     restoreRegisters();
     // ====================================
 
+    checkStubBase(6);
+
 #ifdef TRACKER
     __sync_add_and_fetch(&trackingInfo.secondReturn, 1);
 #endif
 
 #ifdef DEBUG
-    pthread_mutex_lock(&lineReturnedLock);
+    mySpinLock(&lineReturnedLock);
     assert(lineReturned[localLine] == 0);
     lineReturned[localLine] = 1;
-    pthread_mutex_unlock(&lineReturnedLock);
+    mySpinUnlock(&lineReturnedLock);
 #endif
 
     f->output = a->output + b->output;
@@ -219,8 +249,8 @@ startfib(int n, int numthreads)
     createPthreads(numthreads);
 
 #ifdef DEBUG
-    pthread_mutex_init(&lineLock, NULL);
-    pthread_mutex_init(&lineReturnedLock, NULL);
+    mySpinInitLock(&lineLock, NULL);
+    mySpinInitLock(&lineReturnedLock, NULL);
 #endif
 
     Registers saveArea;
@@ -235,8 +265,12 @@ startfib(int n, int numthreads)
     gettimeofday(&startTime, NULL);
 #endif
 
-    fib(a);
+    dprintLine("Start first stacklet: %p->input = %d\n", a, a->input);
 
+    // abandon original stack and start running on a stacklet for fib(a)
+    firstFork(fib, a);
+
+    dprintLine("Finished with first stacklet: %p->output = %d\n", a, a->output);
     // Only one thread can reach here. It does not have to the main thread;
     return a->output;
 }
@@ -254,7 +288,7 @@ main(int argc, char** argv)
            "Will run fib(%d) on %d thread(s)\n\n", n, numthreads);
 
     int x = startfib(n, numthreads);
-    assert(x == result[n]);
+    myassert(x == result[n], "Unexpected Result: %d (should have been %d)\n", x, result[n]);
     printf("*** result ***\n"
            "fib(%d) = %d (verified)\n\n", n, x);
 
