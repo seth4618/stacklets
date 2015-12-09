b34405dcafc93fda0c9e04a6c553e2edd042c391
Preeti U Murthy <preetium@andrew.cmu.edu>
2015-12-10 04:46:30 +0530

    Modified ULI header file to contain only the relevant APIs
    1. u_interrupt.h contains only the relevant APIs for using ULI
    and no code/data structures pertaining to the backend implementation.
    2. Changed the SIM flag to SIM_ULI to avoid any potential conflicts in
    the name
    3. myapp now takes in user argument to specify the number of processors
    to run on

--- sync/system.c
+++ sync/system.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include "system.h"
+#include <stdlib.h>
 #include "u_interrupt.h"
 #include "pthread.h"
+#include "spinlock.h"
+#include "assert.h"
 
 struct mystruct {
     int my_id;  // Per-cpu id
@@ -9,15 +11,18 @@ struct mystruct {
 };
 
 typedef struct mystruct mystruct_t;
-static mystruct_t pcpu[NUM_CORES];
+static mystruct_t *pcpu;
 
 static spinlock_t sl;
 // Returns -1 on error, returns id >=0 otherwise
 static int put_myid(long pid)
 {
     int i;
+    int ncpus;
 
-    for (i = 0; i < NUM_CORES; i++) {
+    ncpus = get_nr_cpus();
+
+    for (i = 0; i < ncpus; i++) {
         if (pcpu[i].pid == -1) {
             pcpu[i].pid = pid;
             return i;
@@ -30,13 +35,15 @@ static int put_myid(long pid)
 //Ideally each pthread must be associated with  cpu
 int get_myid()
 {
-    int i;
+    int i, ncpus;
     pthread_t pid;
 
     pid = (long)pthread_self();
 
+    ncpus = get_nr_cpus();
+
     spinlock_lock(&sl);
-    for (i = 0; i < NUM_CORES; i++) {
+    for (i = 0; i < ncpus; i++) {
         if (pcpu[i].pid == pid)
             goto ret;
     }
@@ -54,13 +61,17 @@ ret:
     return i;
 }
 
-void init_system()
+void init_pcpu_ids(int ncpus)
 {
     int i;
 
-    for (i = 0; i < NUM_CORES; i++) {
+    pcpu = calloc(ncpus, sizeof(mystruct_t));
+    if (!pcpu) {
+        fprintf(stderr, "Cannot allocate memory to pcpu ids\n");
+        exit(1);
+    }
+    for (i = 0; i < ncpus; i++) {
         pcpu[i].my_id = i;
         pcpu[i].pid = -1;
     }
-    init_uint();
 }
